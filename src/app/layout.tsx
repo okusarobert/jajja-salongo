b045a629e0f5b35fb89ed7827d4de165b7c3366b
Wagaana <wagaanaa@gmail.com>
2026-01-01 18:55:54 +0300

    Added Google tag (gtag.js)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16698762120"></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-16698762120');
+          `}
+        </script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
